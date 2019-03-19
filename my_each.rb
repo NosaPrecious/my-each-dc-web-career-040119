741cf4ed7abf2063e7ec008aef0b31070fd63e40
Nosa Okundaye <okundaye.preciousnosa@gmail.com>
2019-03-19 05:13:31 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,8 +1,11 @@
 def my_each(arr) # put argument(s) here
   # code here
+  if block_given?
   count= 0
   while count < arr.length
     yield arr[count]
     count+= 1
   end
+else
+  puts "not called on block"
 end
